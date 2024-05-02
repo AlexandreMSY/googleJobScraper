91633dee6a7d0f6ef0594fad28d98235c64dd0ba
Alexandre Massao <alexandremsubs@gmail.com>
2024-05-02 15:09:20 -0300

    fixes to job description parser prompt message
    fixes to commas in the json template
    this json template issue was leading to bugs in the json string to dict converter

--- main.py
+++ main.py
@@ -11,78 +11,12 @@ load_dotenv(".env")
 descriptionParser = JobDescriptionParser(os.getenv("GEMINI_API_KEY"))
 json = descriptionParser.parseJobDescription(
     """
-        Estágio
-
-        Sobre F1 Commerce
-
-        F1 Commerce é uma empresa que atua no mercado de plataforma de ecommerce há mais de 14 anos no mercado.
-
-        A empresa fornece soluções de ecommerce para empresas de grande porte, atendendo clientes como: JBL, Sanremo, Positivo, Compaq, VAIO, Alpar, Frahm dentre outros.
-
-        Estágio Desenvolvimento FrontEnd HTML/CSS/JS
-
-        A vaga é para atuação no desenvolvimento de interfaces de lojas virtuais.
-
-        Localidade:
-
-        Durante Pandemia:
-
-        Remoto
-
-        Pós Pandemia:
-
-        Flexível
-
-        Nível:
-
-        Estágiário
-
-        Contratação:
-
-        Estágio
-
-        Salário:
-
-        R$ 1.200
-
-        Carga Horária:
-
-        Estágio: 30 horas / semana
-
-        Benefícios:
-
-        » Vale Alimentação ou Vale Refeição
-
-        » Vale Transporte ou estacionamento
-
-        » Seguro de vida
-
-        » Auxílio Home Office (durante pandemia)
-
-        » Desconto em Graduação, Pós-Graduação, Mestrado na UNISINOS
-
-        Atividades:
-        • Codificação de interfaces em HTML/CSS/JS para lojas virtuais a partir de layouts em AI/PSD/PNG
-
-        Requisitos:
-        • Disposição para enfrentar e implementar a criatividade de designers
-        • Superior em curso (independente do semestre) ou curso técnico ligado a área
-        • Capacidade de desenvolvimento HTML sem a utilização de software WYSIWYG
-        • CSS
-        • Software de corte e edição de imagens (Photoshop/Fireworks)
-        • Javascript
-
-        Requisitos obrigatórios:
-        • Comprometimento
-        • Bom humor
-        • Dinamismo
-        • Boa capacidade de relacionamento interpessoal (trabalho em equipe)
-        • Vontade de aprender
-        • Capacidade de concentração e foco na resolução de tarefas
-
-        É Diferencial:
-        • Ter trabalhado em projetos de e-commerce
-        • Conhecimento / Noções de PHP                              
+        Área e especialização profissional: Informática, TI, Telecomunicações - Programador / Desenvolvedor
+        Nível hierárquico: Estagiário
+        Lugar de trabalho: São Paulo, SP
+        Regime de contratação de tipo Estágio
+        Jornada Parcial manhãs
+J       á pensou em estagiar área de TI de uma empresa que é referência em soluções de tecnologia para o mundo corporativo na América Latina? Estamos em busca de um estagiário (a) com perfil para fazer parte do nosso time. Se você possui um perfil dinâmico e flexível, tem vontade de aprender, facilidade para trabalhar em grupo, conhecimento básico em linguagem de programação e inglês avançado. Esta é a oportunidade perfeita para você desenvolver suas habilidades e colaborar com uma empresa sólida e em crescimento acelerado. Suas principais responsabilidades: • Auxiliar na manutenção e desenvolvimento de sistemas (interno ou projetos externos) • Colaborar com a equipe de desenvolvimento para criar e implementar novas funcionalidades. • Participar na resolução de problemas técnicos • Suporte técnico O que você precisa ter tecnicamente: • Graduação em andamento em cursos de: Ciência da Computação, Engenharia da Computação, Análise de Sistemas ou áreas correlatas direcionadas para área de TI. • Conhecimento básico em pelo menos uma das principais linguagens de programação (JS, Java, .Net , Python, Pascal, C++) • Conhecimento em Banco de Dados Relacionais • Inglês avançado ou técnico Nós buscamos estagiários que tenham: • Flexibilidade • Vontade em aprender • Facilidade em trabalhar em grupo • Relacionamento interpessoal Nossos benefícios: - Bolsa-auxílio de R$1.400,00 - Vale-transporte de quantas conduções você precisar sem desconto nenhum; - Vale-refeição flexível (CAJU) de R$35,00/dia para você usar em mercados ou restaurantes - GYMPASS para cuidar do corpo e da mente - Assistência médica Bradesco com 50% de desconto na mensalidade - Assistência Odontológica Sulamérica - DayOff no do seu aniversário Informações adicionais: Horário: 09:00 às 16:00hrs, com 1h de almoço. Localização: Av. Paulista                  
     """
 )
 
